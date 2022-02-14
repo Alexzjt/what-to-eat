17bd51f4804b928949c85574753ef3c5edb2075f
张景涛 <alex_zjt@163.com>
2022-02-14 20:25:10 +0800

    init nestjs

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+
+export default function IndexPage() {
+  return (
+    <div className={styles.titleContainer}>
+      <h1 className={styles.title}>吃什么</h1>
+    </div>
+  );
+}

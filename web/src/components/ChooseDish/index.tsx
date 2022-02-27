f512cc8d5aaf70a285ee0f50b3089e793846b538
张景涛 <alex_zjt@163.com>
2022-02-27 21:21:50 +0800

    checked style

--- web/src/components/ChooseDish/index.tsx
+++ web/src/components/ChooseDish/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { AddCircleOutline } from 'antd-mobile-icons';
+import { AddCircleOutline, CheckOutline } from 'antd-mobile-icons';
 import styles from './index.less';
 import {calcDaysAgo} from '@/utils';
-import {useDispatch} from "@@/plugin-dva/exports";
+import {useDispatch, useSelector} from "@@/plugin-dva/exports";
 export default function (props) {
   const dish = props.dish;
   const dispatch = useDispatch();
+  const {trolley} = useSelector((state: any) => state.category);
+
+  function addOrCheck() {
+    if (trolley.find(d => d.id===dish.id)) {
+      return <span className={styles.addIcon} style={{color: 'green'}}><CheckOutline /></span>;
+    }
+    return <span className={styles.addIcon} onClick={() => {
+      dispatch({type: `category/addDishToTrolley`, payload: dish})
+    }}><AddCircleOutline /></span>
+  }
+
   return (
     <div className={styles.container}>
       <span className='dish-name'>{dish.name}</span>
       <span className='last-eat-time'>{
         dish.lastEatTime ? descriptionHelper(calcDaysAgo(dish.lastEatTime)) : '从未吃过'
       }</span>
-      <span className={styles.addIcon} onClick={() => {
-        dispatch({type: `category/addDishToTrolley`, payload: dish})
-      }}><AddCircleOutline /></span>
+      {addOrCheck()}
     </div>
   );
 }
